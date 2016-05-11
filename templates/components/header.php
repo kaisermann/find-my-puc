77c5266f646f6525978b5729b72d017958b7b3d3
Chris <christian@kaisermann.me>
2016-05-11 03:12:53 -0300

    Mid save, filtros quase prontos

--- templates/components/header.php
+++ templates/components/header.php
@@ -13,11 +13,29 @@
 				<span class="search__label">Destino</span>
 				<span class="search__value"></span>
 			</label>
+			<div class="filters">
+				<span class="filters__title"><strong>Filtros</strong></span>
+				<div class="filters__section">
+					<span class="filters__section__title">Desconsiderar</span>
+					<div class="filters__item">
+						<span class="form-control form-control--radio">
+							<input type="checkbox" name="filter-pathway[]" class="js-radio" id="filter-pathway-stair" value="STAIR_CONNECTION">
+							<label for="filter-pathway-stair">Escadas</label>
+						</span>
+					</div>
+					<div class="filters__item">
+						<span class="form-control form-control--radio">
+							<input type="checkbox" name="filter-pathway[]" class="js-radio" id="filter-pathway-lift" value="LIFT_CONNECTION">
+							<label for="filter-pathway-lift">Elevadores</label>
+						</span>
+					</div>
+				</div>
+			</div>
 			<button for="target" class="btn btn--submit">
 				Me ajuda!
 			</button>
-			<input type="hidden" name="from" class="search-origin">
-			<input type="hidden" name="to" class="search-target">
+			<input type="hidden" name="origin" class="search-origin">
+			<input type="hidden" name="target" class="search-target">
 		</form>
 	</div>
 </aside>
@@ -28,12 +46,38 @@
 		<?php 
 		$query = QUERY_SEARCH_LIST;
 		$result = Neo::$client->run($query);
+		$alphabet = 
+		[
+		"find" => [	  '[áã]', '[éê]', '[íî]', '[óô]', '[úüû]', 'ç', ' - ', '\\.', 'professora?'],
+		"replace" => ['a',    'e',    'i',    'o',    'u',     'c', ' ',   '',    'prof']
+		];
 
 		foreach ($result->records() as $record)
 		{
-			$names = $record->value("names");
-			echo '<li class="searchViewer__item" data-id="'.$record->value("id").'" data-names="'.implode('|', $names).'"><span>';
-			echo $names[0];
+			$names = $record->value('names');
+			$id = $record->value('id');
+
+			$n_names = count($names);
+			$n_alphabet = count($alphabet["find"]);
+			$finalNames = $names;
+
+			for($i = 0; $i < $n_names; $i++)
+			{
+				for($j = 0 ; $j < $n_alphabet; $j++)
+				{
+					$names[$i] = preg_replace('/'.$alphabet["find"][$j].'/imu',$alphabet["replace"][$j],$names[$i]);
+				}
+				$names[$i] = strtolower($names[$i]);
+			}
+			echo '<li class="searchViewer__item" data-id="'.$id.'" data-names="'.implode('|', $names).'"><span class="searchViewer__item__content">';
+			echo '<span class="searchViewer__item__name">'.$finalNames[0].'</span>';
+			if($n_names>1)
+			{
+				for($i = 1; $i < $n_names; $i++)
+					echo '<small>'.$finalNames[$i].($i+1<$n_names?'':'').'</small>';
+			}
+
+
 			echo '</span></li>';
 		}
 		?>
